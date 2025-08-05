aaa2ec9ca91abebefd06dc0a36c195603e5d4414
david <davidmaged1123@gmail.com>
2025-08-05 23:12:48 +0300

    mean to be a commit message for a Git repository.

---
+++ ex2.py
@@ -0,0 +1,9 @@
+import numpy
+
+speed = [99,86,87,88,111,86,103,87,494,78,77,85,86]
+# Convert the list to a numpy array
+speed = numpy.array(speed)
+
+
+x = numpy.mean(speed)
+print(x)
--- exercise1.py
+++ exercise1.py
@@ -1,4 +1,3 @@
 print ("hello 1")
 print ("hello from david2")
-%branch
 print ("hello from branch2")
