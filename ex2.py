198c8bcba50529e5d0b09899ddc62fd208318ac8
david <davidmaged1123@gmail.com>
2025-08-11 19:30:22 +0300

    variance in the data set.

--- ex2.py
+++ ex2.py
@@ -1,9 +1,25 @@
 import numpy
-
+from scipy import stats
 speed = [99,86,87,88,111,86,103,87,494,78,77,85,86]
 # Convert the list to a numpy array
-speed = numpy.array(speed)
+#speed = numpy.array(speed)
 
 
 x = numpy.mean(speed)
-print(x)
+y = numpy.median(speed)
+
+# Calculate the mode using scipy.stats
+mode_ = stats.mode(speed)
+#standard dev
+STD_DEV = numpy.std(speed)
+#Variance 
+variance = numpy.var(speed)
+
+
+#print("Mean speed:", x)
+#print("Median speed:", y)
+#print("Mode speed:", mode_)
+print("Standard Deviation:", STD_DEV)
+print("Variance:", variance)
+
+
