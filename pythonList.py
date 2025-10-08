00857125f4a7f7b3a0bdd4b02320c31506783189
david <davidmaged1123@gmail.com>
2025-10-09 02:19:08 +0300

    access list elements by index

---
+++ pythonList.py
@@ -0,0 +1,5 @@
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+thislist[0] = "orange" ## list is ordered, changeable, allows duplicate values
+print("first item is", thislist[0])
+print("last item is", thislist[-1])
