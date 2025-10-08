b42ebb21eda1607704925fce4f7502c6742b37a5
david <davidmaged1123@gmail.com>
2025-10-09 00:08:49 +0300

    mutible variables

--- exercise3.py
+++ exercise3.py
@@ -1,12 +1,15 @@
-##
-import string
+## Many Values to Multiple Variables
+x ,y,z = 1 ,2 ,3
 
-
-x=float(4)
-y=str(5.5)
-
-
-#print(type(x))
 print(x)
-print(y * 3)
-print(type(y))
+print(y)    
+print(z)
+a = b= c ="bubble"
+print (a)
+print (b)
+#####
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
