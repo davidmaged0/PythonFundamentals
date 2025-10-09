49c220b30003e63e5d08817fe77ad8523261f0f0
david <davidmaged1123@gmail.com>
2025-10-09 20:57:03 +0300

    loop lists

--- pythonList.py
+++ pythonList.py
@@ -1,30 +1,15 @@
-## Change Item Values in a List
+"""thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+  """"""
+   
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-##print(len(thislist))
-#thislist[1:3] = ["blackcurrant", "watermelon", "pear"]
-#To add an item to the end of the list, use the append() method:
-thislist.append("orange") 
-#thislist.append("pineapple")
-#To add an item at the specified index, use the insert() method:
-thislist.insert(1, "orange") 
-print(thislist)
-##Extend List
-thislist2 = ["apple", "banana", "cherry"]
-tropical = ["mango", "pineapple", "papaya"]
-thislist2.extend(tropical)
-print(thislist2)
-##
+
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i += 1
+"""
+
 thislist = ["apple", "banana", "cherry"]
-#thistuple = ("kiwi", "orange")
-#thislist.extend(thistuple)
-#print(thislist)
-#print(type(thislist))
-#To remove an item in a list, use the remove() method:
-#thislist.remove("banana")
-#print(thislist)
-#The pop() method removes the specified index, (or the last item if index is not
-#thislist.pop()
-#thislist.pop(-1)
-#del thislist[0:2]
-thislist.clear()
-print(thislist)
+[print(x) for x in thislist]
