491091c026d84128bbac27d7df60add7e7cd6b8e
david <davidmaged1123@gmail.com>
2025-10-09 18:57:12 +0300

    add to list append ,insert ,and extend

--- pythonList.py
+++ pythonList.py
@@ -1,4 +1,21 @@
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-##print(thislist)
-#thislist[0] = "orange" ## list is ordered, changeable, allows duplicate values
-print(thislist[2:5]) ## from index 2 to index 5 (not included)
+## Change Item Values in a List
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+##print(len(thislist))
+#thislist[1:3] = ["blackcurrant", "watermelon", "pear"]
+#To add an item to the end of the list, use the append() method:
+thislist.append("orange") 
+#thislist.append("pineapple")
+#To add an item at the specified index, use the insert() method:
+thislist.insert(1, "orange") 
+print(thislist)
+##Extend List
+thislist2 = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist2.extend(tropical)
+print(thislist2)
+##
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+print(type(thislist))
