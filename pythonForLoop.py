e4b40f4c916115a9eec9afa7a81403eba0f7fce5
david <davidmaged1123@gmail.com>
2025-10-11 01:12:58 +0300

    shift operator examples

---
+++ pythonForLoop.py
@@ -0,0 +1,9 @@
+# Right Shift Operator
+# The right shift operator (>>) shifts the bits of the number
+#  to the right by the specified number of positions.
+# print(2>>1)# Output: 1
+x = 23 # Binary: 10111
+x<<=4
+print(x)  # Output: 368 (Binary: 101110000)
+x>>=2
+print(x)  # Output: 92 (Binary: 1011100)
