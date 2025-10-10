1854e89181f3be520e80e1b7edc06c1125e54273
david <davidmaged1123@gmail.com>
2025-10-11 01:22:24 +0300

    update tuple by converting to list and back to tuple

--- pythonTuple.py
+++ pythonTuple.py
@@ -1,9 +1,17 @@
-mytuple = ("apple", "banana", "cherry")
-print(mytuple)
-print(len(mytuple))
-singleTuple = ("apple",) 
-print(type(singleTuple))
-x=range(7)
+# mytuple = ("apple", "banana", "cherry")
+# print(mytuple)
+# print(len(mytuple))
+# singleTuple = ("apple",) 
+# print(type(singleTuple))
+# x=range(7)
+# print(x)
+# print(type(x))  
+# print(tuple(x))
+x = ("apple", "banana", "cherry")
+# y = list(x)##casting tuple to list
+# y[1] = "kiwi"##modifying the list
+# y.append("orange")##adding new element to list
+# x = tuple(y)##casting list back to tuple
+Y=("orange","mango")
+x += Y
 print(x)
-print(type(x))  
-print(tuple(x))
