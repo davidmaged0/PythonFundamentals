10dc7a386a2ff330a7d55907e19fc7848e135045
david <davidmaged1123@gmail.com>
2025-10-11 03:11:46 +0300

    unpacking a tuple(assigning the elements of a tuple to variables)

--- pythonTuple.py
+++ pythonTuple.py
@@ -1,17 +1,7 @@
-# mytuple = ("apple", "banana", "cherry")
-# print(mytuple)
-# print(len(mytuple))
-# singleTuple = ("apple",) 
-# print(type(singleTuple))
-# x=range(7)
-# print(x)
-# print(type(x))  
-# print(tuple(x))
-x = ("apple", "banana", "cherry")
-# y = list(x)##casting tuple to list
-# y[1] = "kiwi"##modifying the list
-# y.append("orange")##adding new element to list
-# x = tuple(y)##casting list back to tuple
-Y=("orange","mango")
-x += Y
-print(x)
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
