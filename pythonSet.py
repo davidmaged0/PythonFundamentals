594f31186f583c00a81f6552a358c63de752cf2d
david <davidmaged1123@gmail.com>
2025-10-12 01:41:23 +0300

    pythonSet

---
+++ pythonSet.py
@@ -0,0 +1,10 @@
+# myset = {"apple", "banana", "cherry", 1, False, (1, 2, 3)}
+# print(myset)  # Output: {'apple', 'banana', 'cherry'}
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# print(len(thisset))
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
