16ec50cd0a1b63dd5c9ac4caaea2d4fd957f349f
david <davidmaged1123@gmail.com>
2025-10-16 00:49:44 +0300

    pyrhon strings and slicing

---
+++ pythonStrings.py
@@ -0,0 +1,12 @@
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut "labore" et dolore magna aliqua."""
+print(a)
+a = "Hello, World!"
+print(a[:13])
+print(len(a))
+for x in a:
+    print(x)
+if "ll" in a:
+    print("Yes, 'll' is present.")  
