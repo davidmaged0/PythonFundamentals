4441ced1a4187120ab36e45d2d7e197b4a701912
david <davidmaged1123@gmail.com>
2025-10-19 03:37:33 +0300

    Remove commented string manipulation examples and retain balance calculation and right shift functionality

--- pythonStrings.py
+++ pythonStrings.py
@@ -11,8 +11,22 @@
 # if "ll" in a:
 #     print("Yes, 'll' is present.")  
 #modify string 
-a = " Hello , World  !"
-print(a.upper())
-print(a.lower())
-print(a.strip()) # removes whitespace at the beginning or the end
-print(a.split("o"))
+# a = " Hello , World  !"
+# print(a.upper())
+# print(a.lower())
+# print(a.strip()) # removes whitespace at the beginning or the end
+# print(a.split("o"))
+balance = 100
+tax = 20
+urbalance =f"Your balance is {(balance-tax ):.2f} dollars"
+print(urbalance)
+price =input("Enter the price: ")
+print(f"the price is {'expensive' if float(price)>1000 else 'cheap'}")
+
+txt = "python"
+print(f"i love {txt.upper()}!")
+
+def Rightshift(decimalvalue,bits):
+    return decimalvalue >> bits
+
+print(f"the value after right shift is {Rightshift(8, 2)}")
